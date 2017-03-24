7ab846062d8afa2eaf5f8bc5c901195698a7f7b0
Dustin Burnette <dustinburnette7861@hotmail.com>
2017-03-24 14:32:55 -0500

    Random Number Down just missing the 5 attempts

--- RandomNumberHomework/Program.cs
+++ RandomNumberHomework/Program.cs
@@ -10,6 +10,49 @@ namespace RandomNumberHomework
     {
         static void Main(string[] args)
         {
-        }
+
+            Random random = new Random();
+            int winningNumber = random.Next(1, 101);
+            int parsedGuess = 0;
+            bool answer = false;
+        
+
+            Console.WriteLine("I am thinking of a number between 1-100, can you guess it?");
+
+            while (!answer) {
+
+                string guess = Console.ReadLine();
+
+                if (int.TryParse(guess, out parsedGuess)) {
+
+                    if (parsedGuess > winningNumber)
+                    {
+                        Console.WriteLine("No the number I'm thinking of is lower than " + parsedGuess + " Try again.");
+                    }
+                    else if (parsedGuess < winningNumber)
+                    {
+                        Console.WriteLine("No the number I'm thinking of is higher than " + parsedGuess + " Try again.");
+                    }
+                }
+                else
+                {
+                    answer = true;
+                    Console.WriteLine("You guessed it right!");
+                }
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+            }
     }
 }
